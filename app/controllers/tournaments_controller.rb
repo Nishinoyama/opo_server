0a3a6929720c000bd2b143adf63a0abe442d6542
Nishinoyama <begoingto1414@gmail.com>
2021-07-17 23:56:39 +0900

    add: Tournaments and Player and their Assignments controller
    add: Tournaments and Player and their Assignments controller

---
+++ app/controllers/tournaments_controller.rb
@@ -0,0 +1,10 @@
+class TournamentsController < ApplicationController
+  def index
+    @tournaments = Tournament.all
+    render json: @tournaments
+  end
+  def show
+    @tournament = Tournament.find(params[:id])
+    render json: @tournament
+  end
+end
